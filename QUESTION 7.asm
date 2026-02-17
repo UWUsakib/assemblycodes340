54fc80a26e769e0e53caba0b0d6cf1dbdbeb5098
UWUsakib <150475582+UWUsakib@users.noreply.github.com>
2026-02-17 12:56:51 +0600

    Implement arithmetic operations in QUESTION 7.asm
    Added division and multiplication operations to the assembly code.

--- QUESTION 7.asm
+++ QUESTION 7.asm
@@ -30,7 +30,30 @@
         MOV  CX,5
         MUL  CX         ;answer will be in  DX:AX
                                             ;(h:l)
-                                        
+                                                        
+                                                        
+                                                        
+        ;2) X/Y        16/4
+        
+        MOV AX,16
+        MOV BX,4
+        DIV BX              ; ;answer will be in  AH:AL 
+                                            ;AH=REMAINDER                  
+                                            ;AL=QUOTIENT(ANS)          
+                                            
+            
+        ;3) X*Y/Z       5*6/4
+        
+        
+        MOV AX, 5
+        MOV BX, 6
+        MUL BX
+        
+        
+        MOV CX, 7 
+        DIV CX
+        
+                                            
        
                           
 
@@ -43,6 +66,11 @@
     
     MAIN ENDP
 END MAIN
+
+
+
+
+
 
 
 
